03ba94ffdeae9d3b874ec2884676c966b90a0e1e
Abhishek Thakur <vicky986847@gmail.com>
2024-11-06 14:18:42 +0000

    Added Sample Audio

--- STT.py
+++ STT.py
@@ -5,5 +5,5 @@ speech_to_text = pipeline(model="openai/whisper-large", device="cpu")  # or "cud
 print("Model is initialized...")
 
 # Transcribe audio
-transcription = speech_to_text("path/to/your_audio.wav")
-print(transcription['text'])
+transcription = speech_to_text("stt-test.wav")
+print(transcription)
